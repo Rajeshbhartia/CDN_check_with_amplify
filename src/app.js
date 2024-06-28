6019c5e63028684f636c7a7199cf131274b1d1d3
rajesh-echologyx <rajesh@echologyx.com>
2024-06-28 17:38:55 +0600

    console updated

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ if (process.env.NODE_ENV === "development") {
     document.addEventListener('DOMContentLoaded', function () {
         if (module.hot) {
             module.hot.accept('./components/index', function () {
-                console.log('Accepting the updated mod');
+                console.log('Accepting the updated mod--devlopment');
             });
         }
     });
 } else {
-    console.log('Accepting the updated mod');
+    console.log('Accepting the updated mod--production');
 }
