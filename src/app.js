68a966ec412b1517bc1a96af85a1368f55fa2d41
Rajeshbhartia <rbhartiamuk@gmail.com>
2024-09-03 18:25:10 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,4 +8,12 @@ if (process.env.NODE_ENV === "development") {
     });
 } else {
     console.log('Accepting the updated mod--production');
+
+    var iframe = document.createElement('iframe');
+    iframe.src = "https://main.d1rkp7h1wos0ov.amplifyapp.com/";
+    iframe.width = "100%";
+    iframe.height = "500";
+    iframe.frameBorder = "0";
+    document.body.appendChild(iframe);
+
 }
